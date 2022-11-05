f64478bca084d372b8993c3a09c80d3fd9f560e8
benjiesinzore <benjaminsinzore@gmail.com>
2022-11-05 13:17:28 +0300

    Initial Commit

---
+++ src/main/java/com/sinzore/benjamin/bankingtestapplication/BankingtestapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.sinzore.benjamin.bankingtestapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingtestapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingtestapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinzore/benjamin/bankingtestapplication/BankingtestapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinzore.benjamin.bankingtestapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingtestapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
